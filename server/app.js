dc20c7b0c560b6e00845a8816d33891ceacb0d30
maariyadiminsky <mariya.github@gmail.com>
2021-08-30 11:25:07 -0600

    added types and tested to make sure everything works

--- server/app.js
+++ server/app.js
@@ -5,6 +5,9 @@ const { buildSchema } = require("graphql");
 
 const app = express();
 
+// temporary, pretend db
+let events = [];
+
 // app.use(express.urlencoded({ extended: true }));
 // app.use(express.json());
 
@@ -12,12 +15,26 @@ app.use(bodyParser.json());
 
 app.use("/graphql", graphqlHTTP({
     schema: buildSchema(`
+        type Event {
+            _id: ID!
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
+        input EventInput {
+            title: String!
+            description: String!
+            price: Float!
+        }
+
         type RootQuery {
-            events: [String!]!
+            events: [Event!]!
         }
 
         type RootMutation {
-            createEvent(name: String): String
+            createEvent(eventInput: EventInput): Event
         }
 
         schema {
@@ -26,15 +43,19 @@ app.use("/graphql", graphqlHTTP({
         }
     `),
     rootValue: {
-        events: () => {
-            return [
-                "Romantic",
-                "Cooking",
-                "Sailing"
-            ]
-        },
-        createEvent: ({ name }) => {
-            return name;
+        events: () => events,
+        createEvent: ({ eventInput: { title, description, price } }) => {
+            const event = {
+                _id: Math.random().toString(), // temp, db will generate later
+                title,
+                description,
+                price: +price,
+                date: new Date().toISOString()
+            }
+
+            events = [ ...events, event ];
+
+            return event;
         }
     },
     graphiql: true
