8b2e7d500db13ad2add81637a952df85306979aa
maariyadiminsky <mariya.github@gmail.com>
2021-08-30 09:56:35 -0600

    basic graphql schema test

--- server/app.js
+++ server/app.js
@@ -1,12 +1,43 @@
-import express, { json, urlencoded } from "express";
+const express = require("express");
+const bodyParser = require("body-parser");
+const { graphqlHTTP } = require("express-graphql");
+const { buildSchema } = require("graphql");
 
 const app = express();
 
-app.use(json());
-app.use(urlencoded({ extended: true }));
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
 
-app.get("/", (req, res, next) => {
-    res.send("test");
-});
+app.use(bodyParser.json());
+
+app.use("/graphql", graphqlHTTP({
+    schema: buildSchema(`
+        type RootQuery {
+            events: [String!]!
+        }
+
+        type RootMutation {
+            createEvent(name: String): String
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+        events: () => {
+            return [
+                "Romantic",
+                "Cooking",
+                "Sailing"
+            ]
+        },
+        createEvent: ({ name }) => {
+            return name;
+        }
+    },
+    graphiql: true
+}));
 
 app.listen(3000);
