5080ade7261eb7c5f7c995e4c7d73454b04ab952
maariyadiminsky <mariya.github@gmail.com>
2021-09-10 10:11:01 -0600

    created started Bookings and Events components

--- client/src/pages/App.js
+++ client/src/pages/App.js
@@ -2,14 +2,16 @@ import React from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 
 import Auth from "./Auth";
+import Events from "./Events";
+import Bookings from "./Bookings";
 
 const App = () => {
   return (
     <Switch> 
       <Redirect from="/" to="/auth" exact component={null} />
       <Route path="/auth" component={Auth} />
-      <Route path="/events" component={null} />
-      <Route path="/bookings" component={null} />
+      <Route path="/events" component={Events} />
+      <Route path="/bookings" component={Bookings} />
     </Switch>
   );
 }
