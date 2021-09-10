2904a69be5849ebae0ffb189f9bf89a12a9f8506
maariyadiminsky <mariya.github@gmail.com>
2021-09-10 09:12:39 -0600

    setup tailwind:

---
+++ client/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      container: {
+        center: true,
+      },
+    },
+  },
+  variants: {
+    extend: {
+      opacity: ["disabled"],
+    },
+  },
+  plugins: [],
+}
