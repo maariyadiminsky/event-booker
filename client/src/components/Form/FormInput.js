f4bc2becb1247c91206988f8cfd3f13fa75c2c86
maariyadiminsky <mariya.github@gmail.com>
2021-09-11 15:18:04 -0600

    created initial events page

--- client/src/components/Form/FormInput.js
+++ client/src/components/Form/FormInput.js
@@ -10,7 +10,7 @@ const FormInput = ({ label, input, meta: { touched, submitFailed, error }}) => {
             <label className="block text-white text-2xl font-semibold mb-2">{label}</label>
             <input 
                 type={input.name}
-                className="bg-gray-100 shadow-lg text-green-600 appearance-none text-center text-2xl border rounded w-full py-4 px-3 leading-tight focus:outline-none focus:shadow-outline" {...input} 
+                className="bg-gray-100 shadow-md text-green-600 appearance-none text-center text-2xl border rounded w-full py-4 px-3 leading-tight focus:outline-none focus:shadow-outline" {...input} 
                 autoComplete="off" 
             />
             {hasBeenTouchedAndHasError && <FormError error={error} />}
--- client/src/pages/Events.js
+++ client/src/pages/Events.js
@@ -2,8 +2,17 @@ import React from "react";
 
 const Events = () => {
     return (
-        <div>
-
+        <div className="w-full max-w-3xl mx-auto">
+            <div className="bg-gradient-to-r from-green-400 to-green-300 container shadow-xl rounded px-8 py-8 mt-12">
+                <div className="flex flex-wrap justify-center items-center text-center">
+                    <div className="text-white my-auto text-center text-xl font-semibold">
+                        <span className="text-2xl">+ </span>Create an Event
+                    </div>
+                </div>
+            </div>
+            <div className="container shadow-xl rounded px-8 py-10 mt-6 border-2 border-green-400">
+                An event
+            </div>
         </div>
     );
 }
