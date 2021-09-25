be508f237e82b7080b037a4103d5749baa726409
maariyadiminsky <mariya.github@gmail.com>
2021-09-25 16:06:37 -0600

    created homepage

---
+++ client/src/pages/Home/Home.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import { EVENTS_PATH } from "../../const";
+
+import HomeImage from "./svg/HomeImage";
+
+const Home = () => (
+    <div className="grid md:grid-cols-2 sm:grid-col-1 bg-white overflow-hidden min-h-screen md:pt-32 sm:pt-10">
+        <div className="md:pt-28 md:text-right sm:text-center">
+            <div className="text-green-400 font-bold md:text-7xl sm:text-6xl">
+                Create unique experiences
+            </div>
+            <div className="text-gray-500 font-light md:text-xl sm:text-3xl mt-10">
+                <p>Share and book events at the palm of your hand.</p>
+            </div>
+            <Link to={EVENTS_PATH}>
+                <button className="mt-6 py-3 px-12 text-xl text-white bg-green-400 rounded hover:bg-green-300 transition duration-300">
+                    Start here
+                </button>
+            </Link>
+        </div>
+        <div className="transform sm:scale-125 sm:mt-20 sm:mx-auto">
+            <HomeImage className="transform sm:scale-150"/>
+        </div>
+    </div>
+);
+
+export default Home;
--- client/src/pages/Routes.js
+++ client/src/pages/Routes.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 
 import ProtectedRoute from "../components/Auth/hoc/ProtectedRoute";
-import Home from "./Home";
+import Home from "./Home/Home";
 import Auth from "./Auth";
 import Events from "./Events";
 import Bookings from "./Bookings";
@@ -17,7 +17,7 @@ import {
 const Routes = () => {
 
   return (
-    <div className="bg-gray-100 min-h-screen">
+    <div>
         <Switch>
             <Route path={ROOT_PATH} exact component={Home}/> 
             <ProtectedRoute path={AUTH_PATH} component={Auth} isAuthRoute />
