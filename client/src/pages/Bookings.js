9e8fae61c99af5ec4a92a9c20bf6b2f993c7b564
maariyadiminsky <mariya.github@gmail.com>
2021-09-27 00:20:40 -0600

    load bookings page even if there are no events

--- client/src/pages/Bookings.js
+++ client/src/pages/Bookings.js
@@ -125,6 +125,7 @@ const Bookings = () => {
                     setBookings([]);
                 } else {
                     setEvents([]);
+                    setLoading(false);
                 }
             }
         } catch(err) {
