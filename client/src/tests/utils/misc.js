401757d766751d72f6e182fecedcc206448ef3dd
maariyadiminsky <mariya.github@gmail.com>
2021-12-17 21:57:21 -0800

    fixed comment

--- client/src/tests/utils/misc.js
+++ client/src/tests/utils/misc.js
@@ -3,7 +3,7 @@ export const generateRandomString = () => Math.random().toString(36).substr(2, 5
 
 const originalConsoleLog = console.log;
 export const hideConsoleLog = () => {
-    // note: silence console.log temporarily in apiBaseCall while errors pass
+    // note: silence console.log temporarily in some functions while errors pass
     // without having to turn of --verbose in package.json
     console.log = jest.fn();
 }
